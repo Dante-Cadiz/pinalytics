52aa8b1861ff30bac5f7e4eaecd66a905b1ba78c
Dante-Cadiz <dantecadiz98@mail.com>
2022-02-23 17:48:21 +0000

    add event listener and record score function

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+let frameSubmit = document.getElementById("frame-submit")
+frameSubmit.addEventListener("submit", recordScore);
+
+
+function recordScore(event) {
+  event.preventDefault();
+  let input1 = parseInt(document.getElementById("attempt-1-input").value);
+  let input2 = parseInt(document.getElementById("attempt-2-input").value);
+  console.log('Attempt 1:' , input1);
+  console.log('Attempt 2:' , input2);
+}
+
+
+function calculateFrameScore() {
+
+}
+
+function calculateTotalScore() {
+
+}
+
+function calculateAverage() {
+
+}
+
+function countStrike() {
+
+}
+
+function countSpare() {
+
+}
+
+function incrementStrikeCount() {
+
+}
+
+function incrementSpareCount() {
+
+}
+
+function updateHighScore() {
+
+}
