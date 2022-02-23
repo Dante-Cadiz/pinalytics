57524a00b659932fbaa07eca45fabc51b520a029
Dante-Cadiz <dantecadiz98@mail.com>
2022-02-23 13:35:31 +0000

    initial commit, add boilerplate html

---
+++ assets/scripts/script.js
@@ -0,0 +1,41 @@
+
+
+function logAttempt1Score() {
+
+}
+
+function logAttempt2Score () {
+
+}
+
+function calculateFrameScore() {
+
+}
+
+function calculateTotalScore() {
+
+}
+
+function calculateAverage() {
+
+}
+
+function countStrike() {
+
+}
+
+function countSpare() {
+
+}
+
+function incrementStrikeCount() {
+
+}
+
+function incrementSpareCount() {
+
+}
+
+function updateHighScore() {
+
+}
