4d88f081793ea144ce8055d97e9851f5ae0357a9
Dante-Cadiz <dantecadiz98@mail.com>
2022-03-02 18:37:06 +0000

    update incrementScore function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ let frameSubmit = document.getElementById("frame-submit");
 frameSubmit.addEventListener("submit", countAttempt);
 let frames = 0;
 let maxFrames = 9;
+let totalScore = 0;
 let frameArray = [];
 
 function countAttempt(event) {
@@ -26,7 +27,6 @@ function recordScore() {
   let input1 = document.getElementById("attempt-1-input").value;
   let input2 = document.getElementById("attempt-2-input").value;
   let scoreTable = document.getElementById("score-column");
-  let cumulativeScore = document.getElementById("cumulative-score");
   if (input1 === "X") {
      countStrike();
   } else if (input2 === "/") {
@@ -36,46 +36,58 @@ function recordScore() {
 }
 if (frames === 9) {
   let input3 = document.getElementById("attempt-3-input").value;
+  frameArray.push(input3);
   let scoreHtml = ` <td class="attempt1-score">${input1}</td>
  <td class="attempt2-score">${input2}</td>
  <td class="attempt3-score">${input3}</td>
  `
  scoreTable.innerHTML += scoreHtml;
- let cumulativeHtml = `
- <td colspan="3"></td>
- `
- cumulativeScore.innerHTML += cumulativeHtml;
 } else {
 let scoreHtml = ` <td class="attempt1-score">${input1}</td>
  <td class="attempt2-score">${input2}</td>
  `
  scoreTable.innerHTML += scoreHtml;
- let cumulativeHtml = `
- <td colspan="2"></td>
- `
- cumulativeScore.innerHTML += cumulativeHtml;
 }
 incrementScore();
 }
 
 function incrementScore() {
-  console.log(frameArray);
-  //iterate across the array in here
+  let cumulativeScore = document.getElementById("cumulative-score");
+  let integers = frameArray.map(function (y) {
+    return parseInt(y, 10);
+  });
+  console.log(integers);
+  let last = parseInt(integers.slice(-1));
+  let penultimate = parseInt(integers.slice(-2, -1));
+  
+  if (frames === 9) {
+  let thirdFromLast = parseInt(integers.slice(-3, -2));
+  let mostRecentFrame = thirdFromLast + last + penultimate;
+  totalScore += mostRecentFrame;
+  let cumulativeHtml = `
+ <td colspan="3">${totalScore}</td>
+ `;
+ cumulativeScore.innerHTML += cumulativeHtml;
+  } else {
+  let mostRecentFrame = last + penultimate;
+  totalScore += mostRecentFrame;
+  let cumulativeHtml = `
+ <td colspan="2">${totalScore}</td>
+ `;
+  cumulativeScore.innerHTML += cumulativeHtml;
+  }
 }
 
 
 function countStrike() {
-  let input1 = document.getElementById("attempt-1-input").value;
-  let input2 = document.getElementById("attempt-2-input").value;
            console.log("strike");
-     frameArray.push(input1);
+     frameArray.push("10");
           }
 
 function countSpare() {
   let input1 = document.getElementById("attempt-1-input").value;
-  let input2 = document.getElementById("attempt-2-input").value;
        console.log("spare");
-     frameArray.push(input1, input2);
+     frameArray.push(input1, "10");
 }
 
 function handle10thFrame() {
@@ -95,18 +107,17 @@ function handle10thFrame() {
 function finishGame() {
   let formDiv = document.getElementById("form-wrapper");
   let html = `
-  <button type="button" class="button">Log Game</button>
+  <button type="button" id="button">Log Game</button>
   `
   formDiv.innerHTML = html;
+  // button onclick event  - create function that creates final td section colspan rowspan 2 , with totalScore value in there
 }
 
 function undoLastEntry() {}
 
 function calculateAverage() {
-// take all total scores, add them together, and divide that by number of times played
 }
 
 
 function updateHighScore() {
-//find highest score of logged scores and display it
 }
