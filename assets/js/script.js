054f9e8665435240acf0adb4953d25c87825703d
Dante-Cadiz <dantecadiz98@mail.com>
2022-03-02 12:49:58 +0000

    add function to handle 10th frame and postgame

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,35 @@
 // 'initialise' function to create input fields, so that it is editable on 10th go
 
 let frameSubmit = document.getElementById("frame-submit");
-frameSubmit.addEventListener("submit", recordScore);
 frameSubmit.addEventListener("submit", countAttempt);
 let frames = 0;
 let maxFrames = 9;
 let frameArray = [];
 
-function countAttempt() {
+function countAttempt(event) {
+  event.preventDefault();
   if (frames === 8) {
-    frames++;
     console.log("9 frames bowled");
+    recordScore();
     handle10thFrame();
+    frames++;
   } else if (frames < maxFrames) {
     frames++;
+    recordScore();
     console.log("frame bowled");
   } else {
+    recordScore();
     console.log("10 frames bowled");
+    finishGame();
   }
 }
 
 
-
-// run counter in background script to count attempts, increase counter to 1 for each attempt and and update DOM elements
-// add sequence number in HTML elements to correspond with javascript counter
-
-// target document.getElementById("score-column").children;  (children [0] for first one etc.)
-// for loop matching counter for them
-
-function recordScore(event) {
-  event.preventDefault();
+function recordScore() {
   let input1 = document.getElementById("attempt-1-input").value;
   let input2 = document.getElementById("attempt-2-input").value;
+  let scoreTable = document.getElementById("score-column");
+  let cumulativeScore = document.getElementById("cumulative-score");
   if (input1 === "X") {
      countStrike();
      console.log("strike")
@@ -44,16 +42,28 @@ function recordScore(event) {
   frameArray.push(input1, input2);
 }
 console.log(frameArray);
-let scoreTable = document.getElementById("score-column");
- let scoreHtml = ` <td class="attempt1-score">${input1}</td>
+
+if (frames === 9) {
+  let input3 = document.getElementById("attempt-3-input").value;
+  let scoreHtml = ` <td class="attempt1-score">${input1}</td>
+ <td class="attempt2-score">${input2}</td>
+ <td class="attempt3-score">${input3}</td>
+ `
+ scoreTable.innerHTML += scoreHtml;
+ let cumulativeHtml = `
+ <td colspan="3"></td>
+ `
+ cumulativeScore.innerHTML += cumulativeHtml;
+} else {
+let scoreHtml = ` <td class="attempt1-score">${input1}</td>
  <td class="attempt2-score">${input2}</td>
  `
  scoreTable.innerHTML += scoreHtml;
- let cumulativeScore = document.getElementById("cumulative-score");
  let cumulativeHtml = `
  <td colspan="2"></td>
  `
  cumulativeScore.innerHTML += cumulativeHtml;
+}
 }
 
 
@@ -90,15 +100,25 @@ let input2 = parseInt(document.getElementById("attempt-2-input").value);
 }
 
 function handle10thFrame() {
-
+  let form = document.getElementById("frame-submit");
+     let html = `
+     <label for="attempt-1-input">Attempt 1</label>
+            <input type="text" id="attempt-1-input" name="attempt-1-input" required>
+            <label for="attempt-2-input">Attempt 2</label>
+            <input type="text" id="attempt-2-input" name="attempt-2-input">
+            <label for="attempt-3-input">Attempt 3</label>
+            <input type="text" id="attempt-3-input" name="attempt-3-input">
+            <input type="submit" value="Record Score"></input>
+     `;
+     form.innerHTML = html;
 }
 
-function recordTotalScore() {
-  //once game is completed, record total score to console
-  let finalScore = document.getElementById("final_score").innerHTML;
-  if (frames == maxFrames) {
-     
-  }
+function finishGame() {
+  let formDiv = document.getElementById("form-wrapper");
+  let html = `
+  <button type="button" class="button">Log Game</button>
+  `
+  formDiv.innerHTML = html;
 }
 
 function calculateAverage() {
